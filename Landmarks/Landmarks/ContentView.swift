e5bdf61833e4574cbe11426e74dd1b45d9f47ed4
William Archimede <william.archimede@protonmail.com>
2021-01-30 23:16:02 +0100

    Section 5

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -8,24 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-      VStack(alignment: .leading) {
-        Text("Turtle Rock")
-          .font(.title)
-        HStack {
-          Text("Joshua Tree National Park")
-            .font(.subheadline)
-          Spacer()
-          Text("California")
-            .font(.subheadline)
-        }
+  var body: some View {
+    VStack(alignment: .leading) {
+      Text("Turtle Rock")
+        .font(.title)
+      HStack {
+        Text("Joshua Tree National Park")
+          .font(.subheadline)
+        Spacer()
+        Text("California")
+          .font(.subheadline)
       }
-      .padding()
     }
+    .padding()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
