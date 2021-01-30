cae689c63dad5872ab43f448ad5407941d42146f
William Archimede <william.archimede@protonmail.com>
2021-01-30 22:52:48 +0100

    Section 4

---
+++ Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by William Archimède on 30/01/2021.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+          .clipShape(Circle())
+          .overlay(Circle().stroke(Color.white, lineWidth: 4))
+          .shadow(radius: 7)
+
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
