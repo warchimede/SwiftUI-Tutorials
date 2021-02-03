3eaf37a87811fa13305d08abc0c0ffdc5bb429bd
William Archimede <william.archimede@protonmail.com>
2021-02-04 00:22:14 +0100

    Building List and Navigation, section 1, step 9

---
+++ Landmarks/Landmarks/Landmark.swift
@@ -0,0 +1,33 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by William Archimède on 03/02/2021.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+  var description: String
+  var id: Int
+  var name: String
+  var park: String
+  var state: String
+
+  private var imageName: String
+  var image: Image {
+    Image(imageName)
+  }
+
+  private var coordinates: Coordinates
+  var location: CLLocationCoordinate2D {
+    CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
+  }
+
+  struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+  }
+}
