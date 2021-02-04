7a1b8932d8cfe7c4404ce0e8da9c6f7c2e4f35c0
William Archimede <william.archimede@protonmail.com>
2021-02-04 15:47:46 +0100

    Section 4

---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by William Archimède on 04/02/2021.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+      List {
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+      }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
